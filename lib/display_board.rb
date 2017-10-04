b37f3d756eb00011ceab11054bd8facf117a6a1f
Derek Haller <dhaller.personal@gmail.com>
2017-10-04 19:31:22 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,4 @@
-def display_board (array)
+def display_board (array = ["O", "X", " ", " ", "X", " ", "X", "O", " "])
   puts " #{board[0]} | #{board[1]} | #{board[2]}"
   puts "-----------"
   puts " #{board[3]} | #{board[4] | board[5]}"
